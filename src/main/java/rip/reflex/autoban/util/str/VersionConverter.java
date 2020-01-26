d2ca00b7fd04704715a6fd91faee342e85f57e60
DarksideCode <darksidecode@ya.ru>
2020-01-26 18:37:07 +0200

    1.15 comp. + rework pver->str conversion + upd copyright year

--- src/main/java/rip/reflex/autoban/util/str/VersionConverter.java
+++ src/main/java/rip/reflex/autoban/util/str/VersionConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 DarksideCode
+ * Copyright 2020 DarksideCode
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,12 +43,18 @@ public class VersionConverter {
      * @param protocolVersion The protocol version to transform.
      * @return an easily understandable Minecraft version in X.Y format.
      */
-    public static String pv2str(final int protocolVersion) {
-        return map.getOrDefault(protocolVersion, UNKNOWN);
+    public static String pv2str(int protocolVersion) {
+        int bestMatched = -1;
+
+        for (int pver : map.keySet())
+            if (protocolVersion >= pver)
+                bestMatched = pver;
+
+        return map.getOrDefault(bestMatched, UNKNOWN);
     }
 
     static {
-        // wiki.vg
+        // https://wiki.vg/Protocol_version_numbers
         map.put(  4,  "1.7");
         map.put( 47,  "1.8");
         map.put(107,  "1.9");
@@ -56,6 +62,8 @@ public class VersionConverter {
         map.put(315, "1.11");
         map.put(335, "1.12");
         map.put(393, "1.13");
+        map.put(477, "1.14");
+        map.put(573, "1.15");
     }
 
 }
